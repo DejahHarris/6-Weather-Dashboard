a754e0f876eb23fc9a756d1f5db2d26b99b31031
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-04 12:44:11 -0500

    added local storage functions, and city search and pull city array from local storage functions to script

---
+++ script.js
@@ -0,0 +1,29 @@
+// Global variable declarations
+var cityList = [];
+var cityname;
+// local storage functions
+initCityList();
+initWeather();
+// displays city entered by user
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
+// pulls city array from local storage
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+    
+    renderCities();
+    }
